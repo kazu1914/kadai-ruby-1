d5233c50c84f007ebbf3ed576a0dec659c02a3be
kazuhiro.osugi <kazu1914hiro@outlook.jp>
2021-08-02 04:59:33 +0000

    first commit

---
+++ kadai-introduce.rb
@@ -0,0 +1,13 @@
+myouji = "大杉"
+namae = "和寛"
+desu = "です。"
+puts myouji + namae + desu
+
+nennrei_1 = 8
+nennrei_2 = 10
+nennrei_3 = 6
+saidesu = "歳です。"
+nennrei = nennrei_1 + nennrei_2 + nennrei_3
+puts nennrei.to_s  + saidesu
+
+
