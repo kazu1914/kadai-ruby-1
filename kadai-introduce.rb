b372012a8bff99841d6dabcc202b427bc6eec987
kazuhiro.osugi <kazu1914hiro@outlook.jp>
2021-08-02 07:19:56 +0000

    second commit

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,13 +1,8 @@
 myouji = "大杉"
 namae = "和寛"
-desu = "です。"
-puts myouji + namae + desu
+puts myouji + namae + "です。"
 
-nennrei_1 = 8
-nennrei_2 = 10
-nennrei_3 = 6
-saidesu = "歳です。"
-nennrei = nennrei_1 + nennrei_2 + nennrei_3
-puts nennrei.to_s  + saidesu
+nennrei = 24
+puts nennrei.to_s  + "歳です。"
 
 
